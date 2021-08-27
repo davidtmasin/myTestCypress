9ac95b4365a4783db498c759f633513abf3548a0
DAVID TEIXEIRA DE MASIN <t_davidtm@hapvida.com.br>
2021-08-27 12:28:02 -0300

    Fazendo escolha de um 'option' do HTML

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -3,11 +3,14 @@ describe("Tickets", () => {
 
     //O cypress utiliza da biblioteca Mocka para testes de unidade.
     //O termo only é para teste de unidade, para poder testar somente um único teste
-        it.only("fills all the text input fields", () => {
+    //O comando it é para criar um novo teste.
+    it("fills all the text input fields", () => {
         const firtName = "David";
         const middleName = "Teixeira de"
         const lastName = "Masin";
-        //Identifica através dos seletores CSS
+
+        //Preenchendo os campos input
+        //O comando .get identifica através dos seletores CSS
         cy.get("#first-name")
         .type(firtName); //Add dados textuais
 
@@ -23,5 +26,11 @@ describe("Tickets", () => {
         cy.get("#signature")
         .type(`${firtName} ${middleName} ${lastName}`);
     })
+
+    //Selecionar um option
+    it.only("select two tickets", () => {
+        cy.get("#ticket-quantity").select("2");
+    });
+
     it("has 'TICKETBOX' header's heading", () => {});
 });
