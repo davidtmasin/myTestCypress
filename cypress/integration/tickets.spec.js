9d06116b863e778101565dbe3040172fc5795e08
DAVID TEIXEIRA DE MASIN <t_davidtm@hapvida.com.br>
2021-08-27 12:43:23 -0300

    Interagindo com checkbox

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,9 +1,11 @@
 describe("Tickets", () => {
     beforeEach( () => cy.visit("https://bit.ly/2XSuwCW"));
+    
+    //O comando it é para criar um novo teste.
 
     //O cypress utiliza da biblioteca Mocka para testes de unidade.
     //O termo only é para teste de unidade, para poder testar somente um único teste
-    //O comando it é para criar um novo teste.
+    
     it("fills all the text input fields", () => {
         const firtName = "David";
         const middleName = "Teixeira de"
@@ -33,9 +35,23 @@ describe("Tickets", () => {
     });
 
     //Selecionar um ratio button
-    it.only("select VIP ticket type", () => {
+    it("select VIP ticket type", () => {
         cy.get("#vip")
         .check(); //esta funcionalidade é útil para poder marcar um ratio button
+    });
+
+    //Interagindo com checkboxes
+    it("selects 'social media' checkbox", () => {
+        cy.get("#social-media")
+        .check();
+    });
+    it.only("selects 'friend', and 'publication', then unckeck 'friend'", () => {
+        cy.get("#friend")
+        .check();
+        cy.get("#publication")
+        .check();
+        cy.get("#friend")
+        .uncheck();
     });
 
     it("has 'TICKETBOX' header's heading", () => {});
